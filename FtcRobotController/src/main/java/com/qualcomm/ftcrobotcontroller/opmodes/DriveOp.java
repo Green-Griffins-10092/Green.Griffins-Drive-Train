244915c839acd5a2db0d9b30b278a9bec38a8d56
archerD <dflores0818@gmail.com>
2015-09-03 13:11:31 -0700

    The drive train file, already registered in the opmoderegister

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DriveOp.java
@@ -0,0 +1,32 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by David F. on 9/3/2015.
+ * A simple teleop drive program.
+ */
+public class DriveOp extends OpMode {
+
+    //declare motors
+    DcMotor leftDriveMotor;
+    DcMotor rightDriveMotor;
+
+
+    @Override
+    public void init() {
+        //get the motors from the hardware map named "left_motor" and "right_motor" without quotations.
+        leftDriveMotor = hardwareMap.dcMotor.get("left_motor");
+        rightDriveMotor = hardwareMap.dcMotor.get("right_motor");
+        //reverse a motor so that both motors drive in the same direction
+        rightDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        //Set the motors to the value of the y axis on the appropriate stick.
+        leftDriveMotor.setPower(-gamepad1.left_stick_y);
+        rightDriveMotor.setPower(-gamepad1.right_stick_y);
+    }
+}
